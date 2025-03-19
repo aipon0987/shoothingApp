16c90ed76b1eecfcd0dfc1ac2dff1c41f9f7a2d7
西潟アイ子 <lovebernis1944@gmail.com>
2025-03-19 22:42:14 +0900

    3次実装完了

--- app/src/main/java/com/example/GameActivity.java
+++ app/src/main/java/com/example/GameActivity.java
@@ -11,10 +11,12 @@ import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowInsets;
 import android.view.WindowManager;
 import android.view.WindowMetrics;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -24,11 +26,14 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.myapplication.R;
 
+import java.util.Random;
+
 public class GameActivity extends AppCompatActivity {
 
     private static final int DEFAULT_TIMER_VALUE = 33;
     private static final long TOTAL_TIME = DEFAULT_TIMER_VALUE * 1000;
     private static final long INTERVAL_TIME = 1000;
+    private static final int BALL_SIZE = 80;
 
     public static Intent newIntent(Context context){
         return new Intent(context, GameActivity.class);
@@ -36,8 +41,14 @@ public class GameActivity extends AppCompatActivity {
 
     private FrameLayout mainLayout;
     private ImageView soccerBall;
+    private FrameLayout countDouwnLayout;
+    private TextView countDownTextView;
     private CountDownTimer timer;
     private int count = 0;
+    private int screenWidth = 0 ;
+    private int screenHeight = 0;
+    private int maxXPosition = 0;
+    private int maxYPosition = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,9 +63,48 @@ public class GameActivity extends AppCompatActivity {
 
 
         mainLayout = findViewById(R.id.main);
+        countDouwnLayout = findViewById(R.id.count_down_layout);
+        countDownTextView = findViewById(R.id.count_down_text);
+
+        //画面サイズを取得する
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            WindowMetrics windowMetrics = this.getWindowManager().getCurrentWindowMetrics();
+
+            screenWidth = windowMetrics.getBounds().width();
+            screenHeight = windowMetrics.getBounds().height();
+        } else {
+            WindowManager windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
+            Display disp = windowManager.getDefaultDisplay();
+
+            Point realSize = new Point();
+            disp.getRealSize(realSize);
+            screenWidth = realSize.x;
+            screenHeight = realSize.y;
+        }
+        Log.d("TAG", "screenWidth: " + screenWidth + " screenHeight: " + screenHeight);
+
+        //サッカーボールが表示できる範囲を計算する
+        int  ballSize = dpToPx(BALL_SIZE);
+        maxXPosition = screenWidth - ballSize;
+        maxYPosition = (screenHeight - dpToPx(51)) - ballSize;
+
         LayoutInflater inflater = LayoutInflater.from(this);
         View ballLayout = inflater.inflate(R.layout.soccer_ball_layout, null);
         soccerBall = ballLayout.findViewById(R.id.soccer_ball);
+
+        soccerBall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                count++;
+                removeSoccerBall();
+
+                //もう一度ボールをランダムに生成する
+                Random random = new Random();
+                int leftMargin = random.nextInt(maxXPosition);
+                int topMargin = random.nextInt(maxYPosition);
+                addSoccerBall(leftMargin, topMargin);
+            }
+        });
 
 
         initTimer();
@@ -64,14 +114,15 @@ public class GameActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        int pxSize = dpToPx(80);
-        int leftMargin = dpToPx(100);
-        int topMargin = dpToPx(100);
-        addSoccerBall(leftMargin, topMargin, pxSize);
+        Random random = new Random();
+        int leftMargin = random.nextInt(maxXPosition);
+        int topMargin = random.nextInt(maxYPosition);
+        addSoccerBall(leftMargin, topMargin);
 
     }
 
-    private void addSoccerBall(int leftMargin, int topMargin, int ballSize){
+    private void addSoccerBall(int leftMargin, int topMargin){
+        int ballSize = dpToPx(BALL_SIZE);
         mainLayout.addView(soccerBall);
         ViewGroup.LayoutParams params = soccerBall.getLayoutParams();
         params.width = ballSize;
@@ -79,6 +130,11 @@ public class GameActivity extends AppCompatActivity {
         ((ViewGroup.MarginLayoutParams) params).leftMargin = leftMargin;
         ((ViewGroup.MarginLayoutParams) params).topMargin = topMargin;
         soccerBall.setLayoutParams(params);
+    }
+
+    private void removeSoccerBall(){
+        mainLayout.removeView(soccerBall);
+
     }
 
     private int dpToPx(int dp){
@@ -101,13 +157,11 @@ public class GameActivity extends AppCompatActivity {
                 Log.d("TAG", "OnTick" + second);
 
                 boolean isCountDown = (second - 30) >= 0;
-                if (isCountDown) {
+                if (second -30 == 0) {
 
-                    if (second -30 == 0) {
+                    countDownTextView.setText("スタート");
 
-                    }else  {
-
-                    }
+                }else  if (second - 30 >= 0){
 
                 } else {
 
@@ -117,11 +171,12 @@ public class GameActivity extends AppCompatActivity {
 
             @Override
             public void onFinish() {
-                Intent intent = ClearActivity
-                        .newIntent(GameActivity.this, count);
+                //タップ数の合計を次画面に渡す
+                Intent intent = ClearActivity.newIntent(GameActivity.this, count);
                 startActivity(intent);
 
             }
         };
     }
+
 }
